1292f1dacf430904c4b057a110074a4fbb88498f
Satoshi Shinyashiki <shinyashikis@gmail.com>
2018-08-14 22:44:42 +0900

    modify jupyter settings

--- jupyter-notebook/start.sh
+++ jupyter-notebook/start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+JUPYTER_CONFIG=/root/.jupyter/jupyter_notebook_config.py
+
+PASSWORD=`python -c 'from notebook.auth import passwd;print(passwd())'`
+mkdir -p /root/.jupyter
+echo "c.NotebookApp.password = '$PASSWORD'" > $JUPYTER_CONFIG
+
+jupyter notebook --no-browser --allow-root --ip 0.0.0.0 --notebook-dir $NOTEBOOK_DIR
+
